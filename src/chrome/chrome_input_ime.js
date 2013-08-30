bd5d172930c1d32bcf6c2f2d31ff274d3af87744
Hung-Te Lin <hungte@google.com>
2013-08-30 12:53:00 +0800

    Purify chrome_input_ime module.
    Move UI rendering (that is the only place using jquery) from chrome_input_ime
    to external plugins.

--- src/chrome/chrome_input_ime.js
+++ src/chrome/chrome_input_ime.js
@@ -5,36 +5,83 @@
  * @author hungte@google.com (Hung-Te Lin)
  */
 
-var _debug = true;
-
-function debug(msg) {
-  if (_debug) {
-    console.log(msg);
-  }
-}
-
-chrome_input_ime = function () {
+ChromeInputIME = function () {
   var self = this;
 
-  // internal functions
+  self._debug = true;
+  self.kDefaultEngineId = 'Emulation';
+  self.contextIndex = 0;
 
-  // Engine specific data.
-  self.engineID = 'ChomeOS_Emulation';
-  self.menuitems = [];
-  self.context = null;
-  self.candidate_window = {
-    visible: false,
-    cursorVisible: false,
-    vertical: false,
-    pageSize: 0,
-    auxiliaryText: '',
-    auxiliaryTextVisible:  false,
-    windowPosition: "cursor"
+  self.log = function() {
+    console.log.apply(console, arguments);
+  }
+
+  self.debug = function() {
+    if (self._debug) {
+      self.log.apply(self, arguments);
+    }
+  }
+
+  function CreateDummyUi(name, arg) {
+    return function (ui_context) {
+      self.log("DummyUI:", name, arg, ui_context);
+    }
+  }
+
+  self.ui = {
+    // functions taking engine as input
+    menu: CreateDummyUi("menu", "engine"),
+    candidates_window: CreateDummyUi("candidates_window", "engine"),
+    // functions taking context as input
+    composition: CreateDummyUi("composition", "context"),
+    candidates: CreateDummyUi("candidates", "context"),
   };
 
   function GetContext(contextID) {
-    // TODO(hungte) Support multiple contextes.
-    return self.context;
+    return self.context_list[contextID];
+  }
+
+  function DeleteContext(contextID) {
+    delete self.context_list[contextID];
+  }
+
+  function GetEngineContext() {
+    return self.engineContext;
+  }
+
+  function CreateEngineContext(engineID) {
+    return {
+      engineID: engineID,
+      menuitems: [],
+      candidate_window: {
+        visible: false,
+        cursorVisible: false,
+        vertical: false,
+        pageSize: 0,
+        auxiliaryText: undefined,
+        auxiliaryTextVisible:  false,
+        windowPosition: "cursor"
+      }
+    };
+  }
+
+  function CreateContext(node) {
+    self.contextIndex += 1;
+    return {
+      // InputContext
+      contextID: self.contextIndex,
+      type: 'text',
+
+      node: node,
+      candidates: [],
+
+      composition: {
+        text: '',
+        selectionStart: 0,
+        selectionEnd: 0,
+        cursor: 0
+      }
+    };
   }
 
   function JsKeyCode2Key(k) {
@@ -44,6 +91,14 @@ chrome_input_ime = function () {
     switch (k) {
       case 8:
         return 'Backspace';
+      case 37:
+        return 'Left';
+      case 38:
+        return 'Up';
+      case 39:
+        return 'Right';
+      case 40:
+        return 'Down';
       case 27:
         return 'Esc';
       case 186:
@@ -76,7 +131,6 @@ chrome_input_ime = function () {
   // expected in Chrome Extension input API, so let's make a mini
   // implementation.
   function ImeKeyEvent(ev) {
-    debug(ev);
     return {
       altKey: ev.altKey,
       ctrlKey: ev.ctrlKey,
@@ -87,78 +141,18 @@ chrome_input_ime = function () {
     };
   }
 
-  function CreateNewContext(node) {
-    return {
-      // InputContext
-      contextID: '',
-      type: 'text',
-
-      node: node,
-      candidates: [],
-
-      composition: {
-        text: '',
-        selectionStart: 0,
-        selectionEnd: 0,
-        cursor: 0
-      }
-    };
-  }
-
   function EnterContext(node) {
-    var context = CreateNewContext(node);
-    debug("EnterContext(" + node + ")");
-    context.contextID = node.id;  // TODO(hungte) Replace by a real ID.
-    self.context = context;
+    self.debug("EnterContext:", node);
+    var context = CreateContext(node);
+    self.context_list[context.contextID] = context;
+    self.debug(context);
     return { contextID: context.contextID, type: context.type };
   }
 
-  function LeaveContext() {
-    debug("LeaveContextl()");
-    // TODO(hungte) Really leave console when debug is finished.
-    // self.context = null;
+  function LeaveContext(contextID) {
+    self.debug("LeaveContext()");
+    DeleteContext(contextID);
   }
-
-  function Update() {
-    var context = GetContext();
-    var ui;
-
-    // http://stackoverflow.com/questions/8039182/matching-jquery-text-to-nbsp
-    var nbsp = '\xa0';
-
-    // composition area
-    ui = $('#imePanel #composition');
-    ui.text((context ? context.composition.text : "" )+ nbsp);
-
-    // candidates window
-    ui = $('#imePanel #candidates');
-    ui.empty().append(nbsp);
-    if (context) {
-      context.candidates.forEach(function (item) {
-        var label = item.label || item.id;
-        ui.append(
-            $('<span/>', {text: item.candidate + ' ', "class": "candidate"}).
-            prepend($('<span/>', {text: label, "class": "candidate_label"})));
-      });
-    }
-    if (self.candidate_window.visible) {
-      ui.show();
-    } else {
-      ui.hide();
-    }
-
-    // Update IME UI
-    ui = $('#imePanel #menu');
-    ui.empty();
-    self.menuitems.forEach(function (item) {
-      var label = item.label || item.id;
-      ui.append(
-          $('<li/>', {text: label}).click(function () {
-            DispatchEvent('MenuItemActivated', self.engineID,
-              self.menuitems[$(this).index()].id);
-          }));
-    });
-  };
 
   function SetDefinedParams(dest, src) {
     var i;
@@ -172,14 +166,14 @@ chrome_input_ime = function () {
   // public functions
 
   self.attach = function (node) {
+    var engine = GetEngineContext();
     var keyEventHandler = function(ev) {
       var ev2 = ImeKeyEvent(ev);
-      debug("<attach> " + ev.type + ": " + ev2);
-      var result = DispatchEvent("KeyEvent", self.engineID, ev2);
+      self.debug("<attach>", ev.type, ev2);
+      var result = DispatchEvent("KeyEvent", engine.engineID, ev2);
       if (!result)
         ev.preventDefault();
-      debug("result: " + result);
-      Update();
+      self.debug("result:", result);
       return result;
     };
     node.addEventListener('keydown', keyEventHandler);
@@ -187,7 +181,6 @@ chrome_input_ime = function () {
 
     node.addEventListener('focus', function(ev) {
       var result = DispatchEvent("Focus", EnterContext(node));
-      Update();
       return result;
     });
     node.addEventListener('blur', function(ev) {
@@ -200,29 +193,46 @@ chrome_input_ime = function () {
     });
   };
 
+  self.setUserInterfaceHandlers = function(ui) {
+    self.log("UI handlers changed to:", ui);
+    self.ui = ui;
+  }
+
   // chrome.input.ime API
 
   self.setComposition = function (parameters, callback) {
-    debug('setComposition');
+    self.debug('setComposition');
     var context = GetContext(parameters.contextID);
+    if (!context) {
+      self.debug("Invalid context ID:", parameters.contextID);
+      return;
+    }
     SetDefinedParams(context.composition, parameters,
         'text', 'selectionStart', 'selectionEnd', 'cursor');
-    Update();
+    self.ui.composition(context);
   };
 
   self.clearComposition = function (parameters, callback) {
-    debug('clearComposition');
+    self.debug('clearComposition');
     var context = GetContext(parameters.contextID);
+    if (!context) {
+      self.debug("Invalid context ID:", parameters.contextID);
+      return;
+    }
     context.composition.text = '';
     context.composition.selectionStart = 0;
     context.composition.selectionEnd = 0;
     context.composition.cursor = 0;
-    Update();
+    self.ui.composition(context);
   };
 
   self.commitText = function (parameters, callback) {
-    debug('commitText');
+    self.debug('commitText');
     var context = GetContext(parameters.contextID);
+    if (!context) {
+      self.debug("Invalid context ID:", parameters.contextID);
+      return;
+    }
     var node = context.node;
     node.value = (node.value.substring(0, node.selectionStart) +
                   parameters.text +
@@ -230,18 +240,23 @@ chrome_input_ime = function () {
   };
 
   self.setCandidateWindowProperties = function (parameters, callback) {
-    debug('setCandidateWindowProperties');
-    SetDefinedParams(self.candidate_window, parameters.properties,
+    var engine = GetEngineContext(parameters.engineID);
+    self.debug('setCandidateWindowProperties', parameters);
+    SetDefinedParams(engine.candidate_window, parameters.properties,
         'visible', 'cursorVisible', 'vertical', 'pageSize', 'auxiliaryText',
         'auxiliaryTextVisible', 'windowPosition');
-    Update();
+    self.ui.candidates_window(engine);
   };
 
   self.setCandidates = function (parameters, callback) {
-    debug('setCandidates');
+    self.debug('setCandidates');
     var context = GetContext(parameters.contextID);
+    if (!context) {
+      self.debug("Invalid context ID:", parameters.contextID);
+      return;
+    }
     context.candidates = parameters.candidates;
-    Update();
+    self.ui.candidates(context);
   };
 
   self.setCursorPosition = function (parameters, callback) {
@@ -249,9 +264,10 @@ chrome_input_ime = function () {
   };
 
   self.setMenuItems = function (parameters, callback) {
-    debug('setMenuItems');
-    self.menuitems = parameters.items;
-    Update();
+    self.debug('setMenuItems');
+    var engine = GetEngineContext(parameters.engineID);
+    engine.menuitems = parameters.items;
+    self.ui.menu(engine);
   };
 
   // Currently Chrome implements updateMenuItems in same way as setMenuItems.
@@ -274,7 +290,7 @@ chrome_input_ime = function () {
       document.addEventListener(
           kEventPrefix + event_name,
           function (ime_ev) {
-            debug('on' + event_name);
+            self.debug('on', event_name);
             var result = callback.apply(null, ime_ev.detail);
             if (needEarlyAbort && result) {
               ime_ev.preventDefault();
@@ -303,109 +319,12 @@ chrome_input_ime = function () {
   self.onMenuItemActivated = CreateEventHandler("MenuItemActivated");
   self.onSurroundingTextChanged = CreateEventHandler("SurroundingTextChanged");
   self.onReset = CreateEventHandler("Reset");
-}
 
-// Testing functions
-testContextID = 'input';
-
-function test_setCandidateWindowProperties(flag) {
-  chrome.input.ime.setCandidateWindowProperties({
-    contextID: testContextID,
-    properties: {visible: flag}});
-}
-
-function test_clearComposition() {
-  chrome.input.ime.clearComposition({
-    contextID: testContextID});
-}
-
-function test_setComposition(text) {
-  chrome.input.ime.setComposition({
-    contextID: testContextID,
-    text: text});
-}
-
-function test_commitText(text) {
-  chrome.input.ime.commitText({
-    contextID: testContextID,
-    text: text});
-}
-
-function test_setCandidates(candidate_string) {
-  var i;
-  var items = [];
-
-  for (i in candidate_string) {
-    items.push({
-      candidate: candidate_string[i],
-      id: parseInt(i),
-      label: "" + (parseInt(i) + 1)});
+  // Initialization
+  self.Initialize = function () {
+    self.engineContext = CreateEngineContext(self.kDefaultEngineId);
+    self.context_list = {};
   }
-  chrome.input.ime.setCandidates({
-    contextID: testContextID,
-    candidates: items});
+
+  self.Initialize();
 }
-
-function test_setMenuItems(labels_array) {
-  var i;
-  var items = [];
-  labels_array.forEach(function(label) {
-    items.push({
-      id: 'id_' + label,
-      label: label,
-      style: 'radio',
-    });
-  });
-  chrome.input.ime.setMenuItems({
-    engineID: chrome.input.ime.engineID,
-    items: items});
-}
-
-// jquery-based test init
-$(function() {
-  chrome.input = {
-    ime: new chrome_input_ime
-  };
-
-  // Install IME to test area
-  var node = document.getElementById(testContextID);
-  chrome.input.ime.attach(node);
-
-  if (chrome && chrome.extension) {
-    var croscin = chrome.extension.getBackgroundPage().croscin.instance;
-    var jscin = chrome.extension.getBackgroundPage().jscin;
-
-    // Duplicate log to this page. TODO(hungte) Allow jscin to chain error logs.
-    if (!jscin.log_old) {
-      jscin.log_old = jscin.log;
-    }
-    jscin.log = function (arg) { jscin.log_old(arg); console.log(arg); };
-
-    croscin.set_ime_api(chrome.input.ime, 'emulation');
-    croscin.registerEventHandlers();
-    // croscin has already started, so we need to activate again.
-    chrome.input.ime.DispatchEvent('Activate', 'input_ime');
-
-    $('#TestItems').hide();
-  } else {
-    // Running in simple web page, let's enable all testing buttons.
-    $('#TestItems button').attr("onClick",
-        function () { return "test_" + $(this).text(); });
-    chrome.input.ime.onKeyEvent.addListener(function(engineID, ev) {
-      var val = $('#chkKeyEvent').prop('checked');
-      debug("onKeyEvent(" + engineID + "): " + ev);
-      return !val;
-    });
-    chrome.input.ime.onBlur.addListener(function(contextID) {
-      debug("onBlur(" + contextID + ")");
-    });
-    chrome.input.ime.onFocus.addListener(function(context) {
-      debug(context);
-      debug("onFocus({" + context.contextID + ", " + context.type + "})");
-    });
-    chrome.input.ime.onMenuItemActivated.addListener(function(engineID, menu_id) {
-      debug("menu item activated: id=" + menu_id);
-    });
-  }
-  node.focus();
-})
---
+++ src/chrome/test/testarea.js
@@ -0,0 +1,161 @@
+/**
+ * @fileoverview Description of this file.
+ */
+
+// Testing functions
+testContextID = '1';
+
+function debug() {
+  console.log.apply(console, arguments);
+}
+
+function test_setCandidateWindowProperties(flag) {
+  chrome.input.ime.setCandidateWindowProperties({
+    contextID: testContextID,
+    properties: {visible: flag}});
+}
+
+function test_clearComposition() {
+  chrome.input.ime.clearComposition({
+    contextID: testContextID});
+}
+
+function test_setComposition(text) {
+  chrome.input.ime.setComposition({
+    contextID: testContextID,
+    text: text});
+}
+
+function test_commitText(text) {
+  chrome.input.ime.commitText({
+    contextID: testContextID,
+    text: text});
+}
+
+function test_setCandidates(candidate_string) {
+  var i;
+  var items = [];
+
+  for (i in candidate_string) {
+    items.push({
+      candidate: candidate_string[i],
+      id: parseInt(i),
+      label: "" + (parseInt(i) + 1)});
+  }
+  chrome.input.ime.setCandidates({
+    contextID: testContextID,
+    candidates: items});
+}
+
+// jquery-based test init
+function test_setMenuItems(labels_array) {
+  var i;
+  var items = [];
+  labels_array.forEach(function(label) {
+    items.push({
+      id: 'id_' + label,
+      label: label,
+      style: 'radio',
+    });
+  });
+  chrome.input.ime.setMenuItems({
+    engineID: chrome.input.ime.engineID,
+    items: items});
+}
+
+$(function() {
+  chrome.input = { ime: new ChromeInputIME };
+
+  // Setup UI rendering
+  chrome.input.ime.setUserInterfaceHandlers({
+    menu:
+    function(engine) {
+      // Update IME UI
+      var ui = $('#imePanel #menu');
+      ui.empty();
+      engine.menuitems.forEach(function (item) {
+        var label = item.label || item.id;
+        ui.append(
+            $('<li/>', {text: label}).click(function () {
+              chrome.input.ime.DispatchEvent(
+                  'MenuItemActivated', engine.engineID,
+                  engine.menuitems[$(this).index()].id);
+            }));
+      });
+    },
+
+    candidates_window:
+    function(engine) {
+      // candidates window
+      var ui = $('#imePanel #candidates');
+      if (!engine.candidate_window.visible) {
+        ui.hide();
+      }
+      ui.show();
+    },
+
+    composition:
+    function(context) {
+      // composition area
+      var ui = $('#imePanel #composition');
+      // http://stackoverflow.com/questions/8039182/matching-jquery-text-to-nbsp
+      var nbsp = '\xa0';
+      ui.text((context ? context.composition.text : "" )+ nbsp);
+    },
+
+    candidates:
+    function (context) {
+      var ui = $('#imePanel #candidates');
+      var nbsp = '\xa0';
+      ui.empty().append(nbsp);
+      context.candidates.forEach(function (item) {
+        var label = item.label || item.id;
+        ui.append(
+            $('<span/>', {text: item.candidate + ' ', "class": "candidate"}).
+          prepend($('<span/>', {text: label, "class": "candidate_label"})));
+      });
+    }
+  });
+
+  var node = document.getElementById('input');
+  node.blur();
+  chrome.input.ime.attach(node);
+
+  if (chrome && chrome.extension) {
+    var croscin = chrome.extension.getBackgroundPage().croscin.instance;
+    var jscin = chrome.extension.getBackgroundPage().jscin;
+
+    // Duplicate log to this page. TODO(hungte) Allow jscin to chain error logs.
+    if (!jscin.log_old) {
+      jscin.log_old = jscin.log;
+    }
+    jscin.log = function (arg) { jscin.log_old(arg); console.log(arg); };
+
+    croscin.set_ime_api(chrome.input.ime, 'emulation');
+    croscin.registerEventHandlers();
+    // croscin has already started, so we need to activate again.
+    chrome.input.ime.DispatchEvent('Activate', 'input_ime');
+    $('#TestItems').hide();
+  } else {
+    // Running in simple web page, let's enable all testing buttons.
+    $('#TestItems button').attr("onClick",
+        function () { return "test_" + $(this).text(); });
+    chrome.input.ime.onKeyEvent.addListener(function(engineID, ev) {
+      var val = $('#chkKeyEvent').prop('checked');
+      debug("onKeyEvent(" + engineID + "): " + ev);
+      return !val;
+    });
+    chrome.input.ime.onBlur.addListener(function(contextID) {
+      debug("onBlur(" + contextID + ")");
+    });
+    chrome.input.ime.onFocus.addListener(function(context) {
+      debug(context);
+      debug("onFocus({" + context.contextID + ", " + context.type + "})");
+    });
+    chrome.input.ime.onMenuItemActivated.addListener(function(engineID, menu_id) {
+      debug("menu item activated: id=" + menu_id);
+    });
+  }
+
+  node.focus();
+});
