64f69447e86c9fae135727c1f5ad1c24abf85a56
Hung-Te Lin <hungte@google.com>
2013-10-09 15:04:56 +0800

    nacl/libchewing: Fix pack on release builds.
    Also revised manifest name to make webstore happy.

--- src/nacl/libchewing/pack.sh
+++ src/nacl/libchewing/pack.sh
@@ -27,7 +27,7 @@ fi
 make CONFIG=Release
 ln -s newlib/Release "$NACL"
 rm -f $OUTPUT
-SRCS=$(echo background.{html,js} libchewing/data/*.{dat,tab} jscin/* \
+SRCS=$(echo background.{html,js} libchewing/data/*.{dat,tab} jscin.ext/* \
        _locales/*/* manifest.json $NACL/chewing.nmf $NACL/chewing_[ax]*.nexe)
 for file in $SRCS; do
   [ -f "$file" ] || die "Failed to find $file".
