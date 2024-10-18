3b83c16dab83913c525329164a87466256fdbd54
Hung-Te Lin <hungte@gmail.com>
2024-10-18 16:45:20 +0800

    Add welcome screen (option page) for ChromeOS installations
    When the user first time installs JsCIN on Chromebooks they will
    definitely want to select the preferred input methods in the options
    page.
    
    We don't do the welcome page on non-chromeos devices because emulation
    is by default disabled.

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -12,8 +12,22 @@ import { croscin, jscin } from "./croscin.js";
 globalThis.jscin = jscin
 globalThis.croscin = croscin
 
+async function welcome_chromeos() {
+  chrome.runtime.onInstalled.addListener((event) => {
+    if (event.reason != chrome.runtime.OnInstalledReason.INSTALL)
+      return;
+
+    let option_url = chrome.runtime.getURL(chrome.runtime.getManifest().options_page);
+    chrome.tabs.create({ url: option_url }, function (tab) {
+      console.warn("First time install - welcome!");
+    });
+  });
+}
+
 document.addEventListener( 'readystatechange', function() {
   if (document.readyState === 'complete') {
     croscin.instance = new croscin.IME();
+    if (window.navigator.userAgent.includes(' CrOS '))
+      welcome_chromeos();
   }
 });
