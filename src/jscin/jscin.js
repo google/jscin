a7ef67e4575df2b4caa9e1795e66e6c84f0824ed
Hung-Te Lin <hungte@gmail.com>
2025-08-22 08:45:18 +0800

    Adjust IMKEY_* values to align with XCIN25
    Introduce IMKEY_BELL[2] and move the jscin new values (DELAY, ERROR) to
    a new area that xcin25 didn't use.

--- src/jscin/jscin.js
+++ src/jscin/jscin.js
@@ -12,16 +12,20 @@ const { logger } = AddLogger("jscin");
 export const IMKEY_ABSORB = 0x0;
 export const IMKEY_COMMIT = 0x1;
 export const IMKEY_IGNORE = 0x2;
-export const IMKEY_DELAY  = 0x4;
-export const IMKEY_ERROR  = 0x8;
-export const IMKEY_UNKNOWN = 0x100;
+export const IMKEY_BELL   = 0x4;
+export const IMKEY_BELL2  = 0x8;
+export const IMKEY_ERROR  = 0x20;
+export const IMKEY_DELAY  = 0x21;
+export const IMKEY_UNKNOWN= 0x101;
 
 export var jscin = new InputMethodsEnvironment();
 jscin.IMKEY_ABSORB = IMKEY_ABSORB;
 jscin.IMKEY_COMMIT = IMKEY_COMMIT;
 jscin.IMKEY_IGNORE = IMKEY_IGNORE;
-jscin.IMKEY_DELAY  = IMKEY_DELAY;
+jscin.IMKEY_BELL   = IMKEY_BELL;
+jscin.IMKEY_BELL2  = IMKEY_BELL2;
 jscin.IMKEY_ERROR  = IMKEY_ERROR;
+jscin.IMKEY_DELAY  = IMKEY_DELAY;
 jscin.IMKEY_UNKNOWN= IMKEY_UNKNOWN;
 
 // top level await is not always supported for example Chrome service works so
