a3d95ff484054f60d87ac764ae74428fb73b5e51
Hung-Te Lin <hungte@gmail.com>
2025-08-29 10:08:21 +0800

    Introduce eslint for checking
    Run the eslint to find problems.

---
+++ src/eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  globalIgnores(["external/", "chrome/jquery/"]),
+  {
+    languageOptions: {
+      globals: {
+        chrome: "readonly",
+        process: "readonly",
+      }
+    }
+  },
+  {
+    rules: {
+      "no-unused-vars": ["error", {
+        "varsIgnorePattern": "^debug|warn|error|trace|info|assert|logger|log|jQuery$"
+      }]
+    }
+  }
+]);
