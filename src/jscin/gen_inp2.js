google/jscin

commit 64f063f492fa644f7f42133c727637c88563fc88

gen_inp2: SPACE_AUTOUP only if candidates are less than one page
When multiple pages of candidates are available (usually due to globbing
or partial selection), SPACE_AUTOUP will make it harder for people to
choose the other candidates so following OpenVanilla, the SPACE_AUTOUP
should only work when the number of candidates are less than one page.

src/jscin/gen_inp2.js
@@ -479,22 +479,26 @@ export class GenInp2 extends BaseInputMethod
     }
     this.ShiftState(ctx);
 
+    // In AUTO_COMPOSE mode, the candidates window is already there so most
+    // modern IM implementations will expect the SPACE to commit. Array
+    // rejected this because of the 'quick' that generates a different set of
+    // candidates, but other IMs all towards auto-commit. Boshiamy is more
+    // explicit on SPACE to commit because it has almost no chance to show
+    // candidates more than one page, but the partial match and glob would
+    // change it. OpenVanilla proposed the solution of "auto commit only if
+    // the candidates are <= 1 page", which is a good solution so we'll follow
+    // here.
+    // As a result, the implementation for SPACE here is:
+    //  - If override_autocompose, don't commit.
+    //  - If CanCycleCandidates, don't commit.
+    //  - Otherwise, commit.
     let commit = this.IsSingleCandidate(ctx);
+
     if (!commit && key == ' ' && this.opts.OPT_AUTO_COMPOSE &&
-        !this.override_autocompose) {
-      // In AUTO_COMPOSE mode, the candidates window is already there so most
-      // modern IM implementations will expect the SPACE to commit. Boshiamy
-      // explicit claimed this, Array rejected this (because the 'quick'
-      // generates a different set of candidates), and other IMs are towards
-      // auto commit.
-      // As a result, the implementation for SPACE here is:
-      //  - If override_autocompose, don't commit.
-      //  - If OPT_SPACE_AUTOUP, always commit.
-      //  - If not CanCycleCandidates, commit.
-      if ((this.opts.OPT_AUTO_UPCHAR && this.opts.OPT_SPACE_AUTOUP) ||
-        !this.CycleCandidates(ctx)) {
+      !this.override_autocompose && this.opts.OPT_AUTO_UPCHAR) {
+
+      if (!this.CycleCandidates(ctx))
         commit = true;
-      }
     }
     debug('ConvertComposition', `[${key}]`, commit, this.opts);
 
@@ -602,8 +606,9 @@ export class GenInp2 extends BaseInputMethod
         return this.ResultProcessed(ctx);
 
       case ' ':
-        if ((this.opts.OPT_AUTO_UPCHAR && this.opts.OPT_SPACE_AUTOUP) ||
-            !this.CycleCandidates(ctx)) {
+        if (this.CycleCandidates(ctx))
+          return this.ResultProcessed(ctx);
+        if (this.opts.OPT_AUTO_UPCHAR && this.opts.OPT_SPACE_AUTOUP) {
           this.CommitText(ctx, 0);
           return this.ResultCommit(ctx);
         }

commit df7e42853cffd1d3d057314616b499549768f573

gen_inip2: Process SPACE as selection key in candidates mode
This is a bug introduced by commit 64f063f. For IMs really used SPACE
(not 0) as the selection key (in this test, dayi3p), you can't actually
select the first candidate even if there's only one page. And in fact
you can never select the first candidate because when the SPACE_AUTOUP
is not set. As a result, we need to allow SPACE as selkey, and for such
IMs they have to first press SPACE for conversion, then arrow to cycle
for the other pages.

src/jscin/gen_inp2.js
@@ -506,8 +506,13 @@ export class GenInp2 extends BaseInputMethod
     // candidates more than one page, but the partial match and glob would
     // change it. OpenVanilla proposed the solution of "auto commit only if
     // the candidates are <= 1 page", which is a good solution so we'll follow
-    // here.
+    // here. Hint: try 'yneu' in Boshiamy to check multi-page candidates
+    // behavior.
     let commit = this.IsSingleCandidate(ctx);
+
+    // TODO(hungte) When SPACE is the selection key, actually we can't cycle the
+    // page. We should either always choose 0 in the quirks, or not using SPACE
+    // as the selection when there are multiple pages of candidates.
 
     if (!commit && key == ' ' && this.opts.OPT_AUTO_COMPOSE &&
         !this.CycleCandidates(ctx)) {
@@ -620,6 +625,14 @@ export class GenInp2 extends BaseInputMethod
         return this.ResultProcessed(ctx);
 
       case ' ':
+        // For most cases, SPACE should do CycleCandidates; however for
+        // SELKEY_SHIFT with ' ' really set as the selection key, we do want to
+        // select it.
+        if (this.IsSelectionKey(ctx, key)) {
+          if (this.SelectCommit(ctx, key))
+            return this.ResultCommit(ctx);
+          return this.ResultError(ctx, key);
+        }
         if (this.CycleCandidates(ctx))
           return this.ResultProcessed(ctx);
         if (this.opts.OPT_AUTO_UPCHAR && this.opts.OPT_SPACE_AUTOUP) {